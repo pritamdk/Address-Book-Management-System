5afbefe328254aad44fca465c174c20bf287eb77
pritamdk <pritamdk1994@gmail.com>
2022-06-27 17:44:46 +0530

    Added: Welcome to Address Book Program

---
+++ com/addressbookmanagement/AddressBookManagementSystem.java
@@ -0,0 +1,11 @@
+package com.addressbookmanagement;
+
+public class AddressBookManagementSystem {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Address Book Program ");
+
+	}
+
+}
